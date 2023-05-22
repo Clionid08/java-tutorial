bce85691f07bb44d1a4b2fcc08bdb7b32788b5cd
namuhaja <namuhaja@naver.com>
2023-05-22 23:08:05 +0900

    Method overriding

---
+++ Tutorials/hello/Bike.java
@@ -0,0 +1,24 @@
+
+class Vehicle {
+
+    void run() {
+        System.out.println("Vehicle is running");
+    }
+
+}
+
+public class Bike extends Vehicle {
+    
+    @Override
+    void run() {
+        System.out.println("Bike is running safely");
+
+    }
+
+    public static void main(String[] args) {
+        Bike obj = new Bike();
+
+        obj.run();
+    }
+
+}
